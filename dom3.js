776d3a666e0ac81afac7cae5740c8d87bcc76730
Amitgit1st <aamitp6297@gmail.com>
2023-04-01 00:18:29 +0530

    queryselector

---
+++ dom3.js
@@ -0,0 +1,9 @@
+//QUERYSELECTOR//
+
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+secondItem.textContent='';
+
+
